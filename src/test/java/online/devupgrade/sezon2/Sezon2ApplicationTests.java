99148d01d999bf8c460be6b7f2ab13dc4637c24e
jpilimon <jakub.pilimon@gmail.com>
2021-03-06 11:00:26 +0100

    initial commit

---
+++ src/test/java/online/devupgrade/sezon2/Sezon2ApplicationTests.java
@@ -0,0 +1,13 @@
+package online.devupgrade.sezon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sezon2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
