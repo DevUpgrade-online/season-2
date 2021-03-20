a6036d2ca5cc458b7a63b2db1359f7003a457c42
Robert Jankowski <jankowski.r@hotmail.com>
2021-03-20 02:08:07 +0100

    Tru jpa repos

--- src/main/java/online/devupgrade/sezon2/EcommerceApplication.java
+++ src/main/java/online/devupgrade/sezon2/EcommerceApplication.java
@@ -2,8 +2,10 @@ package online.devupgrade.sezon2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class EcommerceApplication
 {
     public static void main(String[] args) {
